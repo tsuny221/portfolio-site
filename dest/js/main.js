2a1b36ce1db9c561755060b0edd4a14b0f7d9ba3
tsuny221 <kingoftsuny@gmail.com>
2020-10-16 12:31:30 +0900

    [Update] javascript

--- dest/js/main.js
+++ dest/js/main.js
@@ -1,19 +1,21 @@
-window.addEventListener( "scroll" ,function(){
-  let itemHeight = document.getElementById("target").clientHeight;
-  let img = document.getElementById("main__img");
-  //スクロールの高さを取得
-  let scroll = document.body.scrollTop;
+var scroll = document.getElementById("cover");
+//onscrollは監視対象を指定
+scroll.onscroll = function () {
+  // var scroll = document.documentElement.scrollTop || document.body.scrollTop;//スクロール位置取得
+  var scrollHeight = scroll.scrollTop;
+  var itemHeight = document.getElementById("cover-content").clientHeight;
+  var img = document.getElementById("main__img");
+   if( scrollHeight < itemHeight)
+   {
+     img.src = "images/bin.jpg";
+     img.animate([{opacity: '0'}, {opacity: '1'}], 100)
 
-  if( scroll != 0 ){
-    img.src = "images/bin.jpg";
-  }else if( scroll > 1000){
-   img.src = "images/bin.jpg";
-  }else if( scroll > 2000 ){
+   } else if( scrollHeight < itemHeight*2){
+     img.src = "images/usagi.jpg";
+     img.animate([{opacity: '0'}, {opacity: '1'}], 100)
 
-  }else if( scroll > 1000 ){
-
-  }else{
-
-  }
-
-});
+   } else if (scrollHeight < itemHeight * 3) {
+     img.src = "images/bin.jpg";
+     img.animate([{opacity: '0'}, {opacity: '1'}], 100)
+    }
+};
