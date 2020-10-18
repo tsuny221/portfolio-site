9c4be41ad39aa3ae6f16589f273d532060cfedcb
tsuny221 <kingoftsuny@gmail.com>
2020-10-18 17:40:11 +0900

    [Update] design

--- dest/js/main.js
+++ dest/js/main.js
@@ -6,19 +6,19 @@ scroll.onscroll = function () {
   var itemHeight = document.getElementById("cover-content").clientHeight;
   var img = document.getElementById("main__img");
    if( scrollHeight < itemHeight){
-     img.animate([{ opacity: '0' }, { opacity: '1' }], 300);
+    img.animate([{ transform: 'translateY(-50px)' }, { transform: 'none' }], 1000);
      img.src = "images/bin.jpg";
    } else if (scrollHeight < itemHeight * 2) {
-     img.animate([{ opacity: '0' }, { opacity: '1' }], 300);
-     img.src = "images/kiritori.jpg";
+    img.animate([{ transform: 'translateY(-50px)' }, { transform: 'none' }], 1000);
+     img.src = "images/portfolio.jpg";
    } else if (scrollHeight < itemHeight * 3) {
-     img.animate([{ opacity: '0' }, { opacity: '1' }], 300);
+    img.animate([{ transform: 'translateY(-50px)' }, { transform: 'none' }], 1000);
      img.src = "images/kiritori.jpg";
    } else if (scrollHeight < itemHeight * 4) {
-     img.animate([{ opacity: '0' }, { opacity: '1' }], 300);
+    img.animate([{ transform: 'translateY(-50px)' }, { transform: 'none' }], 1000);
      img.src = "images/childrenraku.jpg";
    } else if (scrollHeight < itemHeight * 5) {
-     img.animate([{ opacity: '0' }, { opacity: '1' }], 300);
+     img.animate([{ transform: 'translateY(-50px)' }, { transform: 'none' }], 1000);
      img.src = "images/usagi.jpg";
   }
 };
